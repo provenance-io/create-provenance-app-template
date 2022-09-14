9ee820c671f0b06fb3f290d34e1e643770d3e305
Jarryd Allison <jallison@figure.com>
2022-09-14 11:31:18 -0600

    Its working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import { QRCodeModal, useWalletConnect } from "@provenanceio/walletconnect-js";
 
 function App() {
+  // const { walletConnectService: wcs, walletConnectState } = useWalletConnect();
+
+  // Local state to determine login status. Use redux/react state if desired.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // This function handles login/logout through wcs
+  const handleLogin = () => {
+    if (isLoggedIn) {
+      console.log("Logging out");
+      // wcs.disconnect();
+      setIsLoggedIn(false);
+    } else {
+      console.log("Logging in");
+      // wcs.connect();
+      setIsLoggedIn(true);
+    }
+  };
+
   return (
     <div className="App">
+      {/* Header Navigation Component */}
+      <header className="Header-Nav-Component">
+        {/* Header Logo and Title Component */}
+        <div className="Logo-and-Title-Component">
+          <img src={logo} className="Nav-logo" alt="logo" />
+          Provenance Blockchain Example Application
+        </div>
+        {/* Header Login Component */}
+        <div className="Header-Login-Component">
+          <button className="Login-Button" onClick={handleLogin}>
+            {isLoggedIn ? "Logout" : "Login"}
+          </button>
+        </div>
+      </header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -12,13 +45,18 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://explorer.provenance.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explore Provenance Blockchain
         </a>
       </header>
+      {/* <QRCodeModal
+        walletConnectService={wcs}
+        walletConnectState={walletConnectState}
+        devWallets={["figure_web", "provenance_extension", "provenance_mobile"]}
+      /> */}
     </div>
   );
 }
